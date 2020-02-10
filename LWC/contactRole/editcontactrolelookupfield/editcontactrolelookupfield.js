d07a347c7313aa43b1e1a752250376148206456f
SHADAB ALAM <41892257+shadab4127@users.noreply.github.com>
2020-02-10 15:04:03 +0530

    Update editcontactrolelookupfield.js

--- LWC/contactRole/editcontactrolelookupfield/editcontactrolelookupfield.js
+++ LWC/contactRole/editcontactrolelookupfield/editcontactrolelookupfield.js
@@ -1,1 +1,68 @@
-c
+/* eslint-disable no-alert */
+/* eslint-disable @lwc/lwc/no-async-operation */
+import { LightningElement,track,wire, api } from 'lwc';
+import getContact from '@salesforce/apex/contactRole.getContact';
+const DELAY = 100;
+export default class Editcontactrolelookupfield extends LightningElement {
+    @track search = '';
+    @track contacts;
+    @track message;
+    @api contactIds;
+    @api contactname;
+    @wire(getContact, { searchText: '$search' })
+    wiredContact({ data }) {
+        if (data) {
+            this.contacts = data;
+        } else {
+            this.message ='No record found';
+        }
+    }
+    onfocus(){
+        this.search = '';
+        this.template.querySelector('.slds-dropdown').classList.remove('slds-hide');
+        this.template.querySelector('.slds-dropdown').classList.add('slds-show');
+    }
+    onblure(){
+        this.template.querySelector('.slds-dropdown').classList.remove('slds-hide');
+        this.template.querySelector('.slds-dropdown').classList.add('slds-hide');
+    }
+    handleKeyUp(event) {
+        const searchKey = event.target.value;
+    if (searchKey !== '') {
+        this.template.querySelector('.slds-dropdown').classList.remove('slds-hide');
+        this.template.querySelector('.slds-dropdown').classList.add('slds-show');
+    }
+    else{
+        this.template.querySelector('.slds-dropdown').classList.remove('slds-hide');
+        this.template.querySelector('.slds-dropdown').classList.add('slds-hide');
+    }
+        window.clearTimeout(this.delayTimeout);
+        this.delayTimeout = setTimeout(() => {
+            this.search = searchKey;
+        }, DELAY);
+    }
+    selectContact(event){
+        let name = event.currentTarget.dataset.name;
+        if(name){
+        this.contactname=name;
+        this.contactIds=event.currentTarget.dataset.id;
+        const selectedId = new CustomEvent("selectedcontactid", {
+            detail: this.contactIds
+          });
+          // Dispatches the event.
+        this.dispatchEvent(selectedId);
+        this.template.querySelector('.slds-pill').classList.remove('slds-hide');
+        this.template.querySelector('.contactIcons').classList.add('slds-hide');
+        this.template.querySelector('.slds-combobox').classList.add('slds-hide');
+       }
+        this.contactIds = event.currentTarget.dataset.id;
+        this.template.querySelector('.slds-dropdown').classList.remove('slds-hide');
+        this.template.querySelector('.slds-dropdown').classList.add('slds-hide');
+        this.template.querySelector('.slds-combobox__form-element').classList.add('slds-input-has-border_padding');
+    }
+    removePills(){
+        this.template.querySelector('.slds-pill').classList.add('slds-hide');
+        this.template.querySelector('.contactIcons').classList.remove('slds-hide');
+        this.template.querySelector('.slds-combobox').classList.remove('slds-hide');
+    }
+}
